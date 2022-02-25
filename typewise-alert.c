2b5438fa196f4b25cdac7e8b852447f4cec94570
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-02-25 08:59:48 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -22,16 +22,16 @@ void TempClassification(CoolingType coolingType,int* lowerLimit,int* upperLimit)
 {
    switch(coolingType) {
     case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
+      *lowerLimit = 0;
+      *upperLimit = 35;
       break;
     case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
+      *lowerLimit = 0;
+      *upperLimit = 45;
       break;
     case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
+      *lowerLimit = 0;
+      *upperLimit = 40;
       break;
   }
 }
