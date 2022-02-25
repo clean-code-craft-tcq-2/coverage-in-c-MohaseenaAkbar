f948abe271c08233f3511d61bacabab467d4f771
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-02-25 11:11:22 +0530

    Update and rename Target_alert.c to target_alert.c

--- target_alert.c
+++ target_alert.c
@@ -1,3 +1,5 @@
+#include "typewise-alert.h"
+
 void Targetalert(AlertTarget alertTarget,void (*sendToController)(BreachType),void (*sendToEmail)(BreachType),BreachType breachType)
 {
   switch(alertTarget) {
@@ -12,28 +14,6 @@ void Targetalert(AlertTarget alertTarget,void (*sendToController)(BreachType),vo
 void sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
-}
-void sendToController_Test(BreachType breachType) {
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-  TestCounter_Ver.Controller_Feedback++;
-}
-
-
-void sendToEmail_Test(BreachType breachType) {
-  switch(breachType) {
-    case TOO_LOW:
-      printf("Hi, the temperature is too low\n");
-      TestCounter_Ver.Too_Low_Cnt++;
-      break;
-    case TOO_HIGH:
-      printf("Hi, the temperature is too high\n");
-      TestCounter_Ver.Too_High_Cnt++;
-      break;
-    case NORMAL:
-      TestCounter_Ver.Normal_Cnt++;
-      break;
-  }
 }
   
 void sendToEmail(BreachType breachType) {
