f6f1e505bbf3d5e43c360df0a2cab5f877d55d87
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-02-25 11:21:51 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,23 +1,5 @@
 #include "typewise-alert.h"
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
-    return TOO_LOW;
-  }
-  if(value > upperLimit) {
-    return TOO_HIGH;
-  }
-  return NORMAL;
-}
-
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  TempClassification(coolingType,&lowerLimit,&upperLimit);
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
-}
-
 void checkAndAlert(
     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC,void (*sendToController)(BreachType),void (*sendToEmail)(BreachType)) {
     BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
