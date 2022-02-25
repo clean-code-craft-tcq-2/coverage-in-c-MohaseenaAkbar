3bd5d591ebc85cbd89cc75eef5509258e9bd4983
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-02-25 09:13:14 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -38,6 +38,6 @@ void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
 void sendToEmail_Test(BreachType breachType);
 void alertTarget(AlertTarget alertTarget,void (*sendToController)(BreachType),void (*sendToEmail)(BreachType),BreachType breachType);
-void TempClassification(CoolingType coolingType,int* lowerLimit,int* upperLimit);
+void TempClassification(CoolingType coolingType,int *lowerLimit,int *upperLimit);
 void sendToController_Test(BreachType breachType);
 void Reset_Counter();
